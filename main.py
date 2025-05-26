22bc9e5b32c53dcb67381e14fe83da663dc20e74
Bhagavath Achani <bhagavathachani@Mac.home>
2025-05-26 10:57:41 +0200

    find duplicate files in a folder

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cli.interface import app
 from pathlib import Path
 from organizer.scanner import Scanner
 from config import load_config
+from collections import defaultdict
+
 
 
 def run(source: Path, dest: Path, dry_run: bool) -> None:
@@ -22,10 +24,38 @@ def run(source: Path, dest: Path, dry_run: bool) -> None:
 
     scanner = Scanner(source)
     files = scanner.scan()
+    
+    # we are grouping files by size
+    size_map = defaultdict(list)
+    for file in files:
+        size_map[file.size].append(file)
+    
+    # now we grouping based on hashes, aka same content
+    duplicates = []
+    for size, group in size_map.items():
+        if len(group) > 1:
+            hash_map = defaultdict(list)
+            for file in group:
+                file_hash = file.compute_hash()
+                hash_map[file_hash].append(file)
+                
+            for dup_group in hash_map.values():
+                if len(dup_group) > 1:
+                    duplicates.append(dup_group)
+                    
+    if duplicates:
+        print("Found duplicate files:")
+        for group in duplicates:
+            print(" - Duplicate set:")
+            for file in group:
+                print(f"{file.path}")
+    else:
+        print("No duplicates found.")
+        
 
-    print(f"Found {len(files)} files.")
-    for file in files[:10]:
-        print(file)
+    # print(f"Found {len(files)} files.")
+    # for file in files[:10]:
+    #     print(file)
 
 
 def main():
