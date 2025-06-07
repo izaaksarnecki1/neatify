6eb3a6a376757d1afc5e1d1d2da48d2a11065de1
Izaak Sarnecki <izaaksarnecki@outlook.com>
2025-06-07 21:51:13 +0200

    clean main

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ from organizer.scanner import Scanner
 from config import load_config
 from organizer.duplicates import process_duplicates
 
-def run(source: Path, dest: Path, dry_run: bool, delete_mode: str, check_duplicates: bool) -> None:
-    # function where main logic will be placed. Called from interface.py
-    # print(f"Source: {source}")
-    # print(f"Destination: {dest}")
-    # print(f"Dry run: {dry_run}")
 
+def run(source: Path, dest: Path, dry_run: bool, delete_mode: str, check_duplicates: bool) -> None:
     config = load_config()
     if source:
         config["source"] = source
@@ -22,9 +18,7 @@ def run(source: Path, dest: Path, dry_run: bool, delete_mode: str, check_duplica
         config["dest"] = dest
 
     config["dry_run"] = dry_run
-
-    if check_duplicates:
-        print(f"Checking for duplicate files in: {source}\n")
+    config["check_duplicates"] = check_duplicates
 
     scanner = Scanner(config)
     classifier = Classifier(config["categories"])
@@ -36,17 +30,12 @@ def run(source: Path, dest: Path, dry_run: bool, delete_mode: str, check_duplica
         file.category = classifier.classify(file)
 
     organizer.organize_all(files)
-    
+
     scanner = Scanner(source)
     files = scanner.scan()
 
     if check_duplicates:
         process_duplicates(files, dry_run, delete_mode)
-    
-    
-    # print(f"Found {len(files)} files.")
-    # for file in files[:10]:
-    #     print(file)   
 
 
 def main():
